92248479bf9e02ce314e126f443f2efe60a12078
Le Nhut Thai <thaidc25v7k029@vlvh.ctu,edu,vn>
2026-08-16 09:37:34 +0700

    Cai dat hieu chinh contact

--- public/index.php
+++ public/index.php
@@ -67,7 +67,7 @@ $pages = $paginator->getPages(length: 3);
                 <td><?= html_escape(date('d-m-Y', strtotime($contact->created_at))) ?></td>
                 <td><?= html_escape($contact->notes) ?></td>
                 <td class="d-flex justify-content-center">
-                  <a href="<?= 'edit.php' ?>"
+                  <a href="<?= '/edit.php?id=' . $contact->id ?>"
                     class="btn btn-xs btn-warning">
                     <i alt="Edit" class="fa fa-pencil"></i> Edit</a>
                   <a href="#" class="btn btn-xs btn-danger ms-1">
--- src/classes/Contact.php
+++ src/classes/Contact.php
@@ -113,7 +113,7 @@ class Contact
   {
     $result = false;
 
-    if ($this->id = 0) {
+    if ($this->id > 0) {
       $statement = $this->db->prepare(
         'update contacts set name = :name,
             phone = :phone, notes = :notes, updated_at = now()
@@ -144,5 +144,16 @@ class Contact
     }
 
     return $result;
+  }
+
+  public function find(int $id): ?Contact
+  {
+    $statement = $this->db->prepare('select * from contacts where id = :id');
+    $statement->execute(['id' => $id]);
+    if ($row = $statement->fetch()) {
+      $this->fillFromDbRow($row);
+      return $this;
+    }
+    return null;
   }
 }
